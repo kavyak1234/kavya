63d66f5e4d881f47f9d67b8e55d93ff4349ff1dc
python <kavyakkavyak153@gmail.coma>
2025-03-17 13:33:51 +0530

    initial command

--- python.py
+++ python.py
@@ -447,6 +447,67 @@
 # set1 = {1, 2, 3, 4, 5, 6}
 # for i in set1:
 #     print(i),
+# for i in range(1,5):
+#     print("*"*i)
+# for i in range(1,10):
+#     print("*"*i)
+# for i in range(4,0,-1):
+    
+#     print("*"*i)
+# for i in range(1,5):
+#     print("a"*i)
+# for i in range(1,5):
+#     print("1"*i)
+# for i in range(4):
+#     for j in range(4):
+#         print(i)
+# for i in range(4):
+#     for j in range(4):
+#         print(j,end='')
+#     print()
+#for i in range(4):
+#         print(i,end='')
+#     for j in range(4):
+#     print()
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print(i,end='')
+#     print()
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print(j,end='')
+#     print()
+# for i in range(0,5):
+#     for j in range(0,5):
+#         print(j,end='')
+#     print()
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="")
+#         a+=1
+#     print()
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="")
+#         a+=2
+#     print()
+# a=3
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end="")
+#         a+=3
+#     print()
+a=3
+for i in range(4):
+    for j in range(1,a+1):
+        if i%2==0:
+            print(j-1,end="")
+        else:
+            print(a-j,end="")   
+    print()
+
 
 
 
