b307fc2faeef46d46b6131fc68278d34a60130a1
python <kavyakkavyak153@gmail.coma>
2025-03-17 11:38:43 +0530

    initial command

---
+++ python.py
@@ -0,0 +1,472 @@
+#print(123)
+# print("kavya")
+# Roll_no=3
+## Name="kavya"
+# weight=45.5
+# print(Roll_no)
+# print(Name)
+# print(weight)
+# print('roll_number:',Roll_no)
+# print('Name:',Name)
+# print('weight:',weight
+
+
+# a=int(input('Enter the number between 1 to 7'))
+# if a==1:
+#     print("sunday")
+# elif a==2:
+#     print("monday")
+# elif a==3:
+#     print("tuesday")
+# elif a==4:
+#     print("wednesday")
+# elif a==5:
+#     print("thurday")
+# elif a==6:
+#     print("friday")
+# elif a==7:
+#     print("satarday")
+# else:
+#     print("the days are not defined")
+# a=input("enter the city")
+# if a=="delhi":
+#     print("Red fort")
+# elif a=="AGRA":
+#     print("taj mahal")
+# elif a=="jaypur":
+#     print("jal mahal")
+# else:
+#     print("the city is not defined")
+# a=int(input('enter a number'))
+# b=a%10
+# print(b)
+# if b % 3==0:
+#     print('the last digit is divisible by 3')
+# else:
+#     print("not divisible")
+# a=int(input("bike cost price"))
+# if a>100000:
+#     tax=0.15*a
+#     print("road tax to be paid",tax)
+# elif a>50000:
+#     tax=0.10*a
+#     print("road tax to be paid",tax)
+# else:
+#     tax=0.05*a
+#     print("road tax is not paid",tax)
+# for i in range (1,11,):
+#     #print(i)
+#     print("hello")
+#     a=int(input("Enter starting"))
+#     b=int(input("Enter ending"))
+#     for i in range(a,b+1):
+#         print(i)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"
+# sum=0
+# for i in range(a,b+1):
+#     #sum=sum+i,
+#     sum+=i
+# print(sum)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# sum=0
+# for i in range(a,b+1):
+#     #sum=sum+i,
+#     sum+=i
+# print("sum=",sum)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# product=1
+# for i in range(a,b*1):
+#     #product=product*i,
+#     product*=i
+# print("product=",product)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# for i in range(a,b+1):
+#     if i%2==1:
+#         print(i)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# sum=0
+# for i in range(a,b+1):
+#     if i%2==1:
+#      sum=sum+i
+# print("sum=",sum)
+# a=int(input("Enter starting"))
+# b=int(input('Enter ending'))
+# sum1=0
+# sum2=0
+# sum3=0
+# for i in range(a,b+1):
+#   if i % 2==0:
+#     sum1+=i
+    
+#   else:
+#     sum2+=i
+#   sum3=sum3+i
+
+# print(sum1,sum2,sum3,)
+# a=int(input("Enter a number "))
+# product=1
+# for i in range(1,a+1):
+#       product=product*i
+# print("product",product)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# for i in range(a,b+1):
+#   if i%3==0:
+#     print(i)
+# a=int(input("Enter a number"))
+# for i in range(1,11):
+#   print(i,"*",a,"=",i*a)
+# rev=""
+# a="python"
+# for i in a:
+#   rev=i+rev
+#   print(rev)
+# a=int(input("Enter your age"))
+# if a>=18:
+#   print("Eligible for voting")
+# else:
+#   print("Not eligible for voting")
+# a=int(input("Enter first number"))
+# if a%2==0:
+#   print('Number is even')
+# else:
+#   print("Number is odd")
+# a=int(input("Enter any number between 1 to 7"))
+# if a==1:
+#   print("Januvary")
+# elif a==2:
+#   print("februvary")
+# elif a==3:
+#   print("March")
+# elif a==4:
+#   print("April")
+# elif a==5:
+#   print("May")
+# elif a==6:
+#   print("June")
+# elif a==7:
+#   print("July")
+# elif a==8:
+#   print("August")
+# elif a==9:
+#   print("September")
+# elif a==10:
+#   print("October")
+# elif a==11:
+#   print("November")
+# elif a==12:
+# a=int(input("Enter any number"))
+# print("lasst digit of the number is")
+# marks=int(input("enter marks"))
+# if marks>90:
+#     print("grade:A")
+# elif marks>80:
+#     print("grade B")
+# elif marks>70:
+#     print("grade C")
+# i=0
+# while i<=10:
+#     print(i)
+#     i+=1
+# i=0
+# while i<=10:
+#     print(i)
+#     i+=1
+# i=1
+# while i<=10:
+#     print("hello")
+#     i+=1
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# i=1
+
+# while i<=10:
+#     print(i)
+#     i+=1
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# sum=0
+# while a<=b:
+#      #sum=sum+i,
+#      sum+=a
+#      a+=1
+#      print("sum",sum)
+# a=int(input("Enter starting "))
+# b=int(input("Enter ending"))
+# product=1
+# while a<=b:
+#       product=product*a
+#       a+=1
+# print("product",product)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# while a<=b:
+#     if a%2==1:
+#         print(a)
+#     a+=1
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# while a<=b:
+#     if a%3==1:
+#         print(a)
+#     a+=1    
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# while a<=b:
+#     if a%2==0:
+#         print(a)
+#     a+=1
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# sum=0
+# while a<=b:
+#     if a%2==1:
+#         sum=sum+a
+#     a+=1
+# print(sum)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# sum=0
+# while a<=b:
+#     if a%2==0:
+#         sum=sum+a
+#     a+=1
+# print(sum)
+# a=int(input("Enter starting"))
+# b=int(input('Enter ending'))
+# sum1=0
+# sum2=0
+# sum3=0
+# while a<=b:
+#     if  a%2==0:
+#         sum1+=a
+#     else:
+#         sum2+=a
+#     sum3+=a
+#     a+=1
+# print(sum1,sum2,sum3)
+# a=int(input("Enter starting"))
+# b=int(input("Enter ending"))
+# while a<b:
+#     if a%3==0:
+#         print(a)
+#     a+=1
+# i=1
+# a=int(input("Enter a number"))
+# while i<=10:
+#     print(i,"*",a,"=",i*a)
+#     i+=1
+# num=123
+# rev=0
+# while num>0:
+#     digit=num%10
+#     rev=rev*10+digit
+#     num=num//10
+#     print("Reversed number",rev)
+# num=123
+# a=0
+# while num>0:
+#     a+=1
+#     num//=10
+# print("Number of digits",a)
+# a=123
+# rev=0
+# while a>0:
+#     d=a%10
+#     # print(d)
+#     a=a//10
+#     rev=(rev*10)+d
+# print(rev)
+# a=123
+# count=0
+# while a>0:
+#     count+=1
+
+#     a//=10
+# # print("Number of digits",count)
+# a=int(input("Enter a number"))
+# sum=0
+# while a>0:
+#     d=a%10
+#     a//=10
+#     print(d)
+#     sum=sum+d
+# print("sum of digits",sum)
+ # @"Exercises"
+
+# "assignment operators"
+# a=10
+# a=a-12
+# a+=12
+# print('a:',a)
+# a=10
+# b=5
+# if a>b:
+#     if a <20:
+#         print("value greater than 5 and less than 20")
+# #arithematic operators
+
+# a=7
+# b=2
+# #addition 
+
+# print('sum:',a+b)
+# #substration
+
+# print('sum',a-b)
+# #multiplication
+
+# print(a * b) 
+# #division
+
+# print(a / b) 
+# #integer division
+# result = 10 // 3   
+# print(result)
+# #modulo
+
+# print(a % b) 
+# #exponentiation
+# print(a ** b) 
+#assignment  operators
+
+# length = 15
+# length += 5  
+# print(length)  
+#logical operators
+#logical and
+
+# a = 7
+# condition = a > 5 and a < 10  
+# print(condition) 
+#logical or
+
+# a = 7
+# condition = a > 10 or a < 5  
+# print(condition)
+#logical not
+
+# a = 7
+# condition = not(a > 5)  
+# print(condition)  
+#comparison operators
+
+# height1 = 12
+# height2 = 14
+# print(height1 < height2)
+
+#condition statements
+#if
+# age = 20
+# if age >= 18:
+#     print("Eligible to vote.")
+
+#if else
+# age = 10
+
+# if age <= 12:
+#     print("Travel for free.")
+# else:
+#     print("Pay for ticket.")
+
+#elif statement
+# age = 25
+
+# if age <= 12:
+#     print("Child.")
+# elif age <= 19:
+#     print("Teenager.")
+# elif age <= 35:
+#     print("Young adult.")
+# else:
+#     print("Adult.")
+
+#nested if 
+# age = 70
+# is_member = True
+
+# if age >= 60:
+#     if is_member:
+#         print("30% senior discount!")
+#     else:
+#         print("20% senior discount.")
+# else:
+#     print("Not eligible for a senior discount.")
+
+#if else if ladder
+
+# a=10
+# if a==0:
+#   print('zero')
+# elif a>=1:
+#    print("its a positive number")
+# elif a<0:
+#    print("its a negative number")
+
+#while loop
+# cnt = 0
+# while (cnt < 3):
+#     cnt = cnt + 1
+#     print("Hello Geek")
+
+#else block while loop
+# cnt = 0
+# while (cnt < 3):
+#     cnt = cnt + 1
+#     print("Hello Geek")
+# else:
+#     print("In Else Block")
+
+#for loop
+# n = 4
+# for i in range(0, n):
+#     print(i)
+
+#list,tuple,string,Dictionary
+# li = ["geeks", "for", "geeks"]
+# for i in li:
+#     print(i)
+    
+# tup = ("geeks", "for", "geeks")
+# for i in tup:
+#     print(i)
+    
+# s = "Geeks"
+# for i in s:
+#     print(i)
+    
+# d = dict({'x':123, 'y':354})
+# for i in d:
+#     print("%s  %d" % (i, d[i]))
+    
+# set1 = {1, 2, 3, 4, 5, 6}
+# for i in set1:
+#     print(i),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
