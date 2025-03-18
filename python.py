b4665c97f8de31fb1c59e2b167cfd074a18a7fbc
python <kavyakkavyak153@gmail.coma>
2025-03-18 13:33:06 +0530

    initial command

--- python.py
+++ python.py
@@ -499,23 +499,89 @@
 #         print(a,end="")
 #         a+=3
 #     print()
-a=3
-for i in range(4):
-    for j in range(1,a+1):
-        if i%2==0:
-            print(j-1,end="")
-        else:
-            print(a-j,end="")   
-    print()
+# a=3
+# for i in range(4):
+#     for j in range(1,a+1):
+#         if i%2==0:#add odd and even row
+#             print(j-1,end="")
+#         else:
+#             print(a-j,end="")   
+#     print()
 
 
+# a=3
+# for i in range(4):
+#     for j in range(1,a+1):
+#         if i%2==0:#add odd and even row
+#             print(j,end="")
+#         else:
+#             print(a-j,end="")   
+#     print()
+
+# a=3
+# for i in range(4):
+#     for j in range(1,a+1):
+#         if i%2==0:#add odd and even row
+#             print(j,end="")
+#         else:
+#             print(a,end="")   
+#     print()
 
 
+# for i in range (1,4):
+#     for j in range (i):
+#         print(j+1,end="")
+#     print()
 
 
+# for i in range (1,11):
+#     for j in range (i):
+#         print(j+1,end=" ")
+#     print()
 
 
+# for i in range (1,4):
+#     for j in range (i):
+#         print(i,end=" ")
+#     print()
 
+# a=1
+# for i in range (1,4):
+#     for j in range (i):
+#         print(a,end=" ")
+#         a+=1
+#     print()
+
+
+# a=1
+# for i in range (1,4):
+#     for j in range (i):
+#         print(a,end=" ")
+#         a+=2
+#     print()
+
+
+# a=3
+# for i in range (5):
+#     for j in range (i):
+#         if i%2==1:
+#             print(j,end=" ")
+#         else:
+#             print(i-j-1,end=" ")     
+        
+#     print()
+
+# for i in range(3):
+#     a=65
+#     for j in range(3):
+#         print(chr(a),end=" ")
+#         a+=1
+#     print()
+# A=65
+# for i in range(3):
+#     for j in range(3):
+#         print((A+i),end=" ")
+#     print()
 
 
 
