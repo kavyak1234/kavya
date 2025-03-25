b690f1da6782c471e7f337363e919a7f4be6b93a
python <kavyakkavyak153@gmail.coma>
2025-03-25 13:28:20 +0530

    initial command

--- index.py
+++ index.py
@@ -105,7 +105,8 @@
 # l1=l.copy()
 # l.pop()
 # print(l1)
-# print(l)
+# print(l)t.append 
+# print(t)
 
 #l.clear()
 
@@ -120,10 +121,70 @@
 # a=int(input('enter first number'))
 # b=int(input('enter  second number'))
 # l.extend([a,b])
+# print(l)t.append 
+# print(t)
+# l=[1,2,3,4,5,6,7]
+# odd=[i for i in l if i%2==1]
+# even=[i for i in l if i%2==0]
+# print(odd)
+# print(even)
+
+# t=(1,2,3,4,5,6,7)
+# print(t)
+# print(t[0])
+
+# for i in t:
+#     print(i)
+
+# if 6 in t:
+#     print('yes')
+
+# else:
+#     print('no')
+
+# t=(1,2,[11,12,13])
+# # # print(t)
+# # print(t[2])
+# t(2).append(14)
+# print(t)
+
+# l=[1,2,[10,11,12]]
+# l[2].append(13)
 # print(l)
 
-l=[]
-a=int(input('starting number'))
-b=int(input('ending number'))
-l.extend([a-b])
-print(l)
+# l=[1,[10,[100,200]],2,3]
+# # for i in l:
+# # print(l[1])
+# # l[1].append(20)
+# # print(l)
+
+# l[1].append(300)
+# l[1].append(20)
+# # print(l)
+
+# t=(1,2,3,4)
+# print(t.index(3))
+
+# l=[1,[10,[100,200]],2,3]
+# print(l[1][1])
+
+# l=[1,2,3]
+# t=tuple(l)
+# print(l)
+
+# t=[1,2,3]
+# l=list(t)
+# print(t)
+
+# a=1,2,3,4,5
+# print(a)
+
+# t=(1)
+# print(t)
+# print(type(t))
+
+t=(1,2,3)
+l=list(t)
+l.append(4)
+t=tuple(l)
+print(t)
