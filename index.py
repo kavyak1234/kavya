ea7822a9cb11d761a55c21cf46a1a8117a8434fe
python <kavyakkavyak153@gmail.coma>
2025-04-01 13:23:14 +0530

    initial command

--- index.py
+++ index.py
@@ -414,17 +414,60 @@
 #     print(i)
 
 
-l=[]
-a=int(input('number of students'))
-for i in range(0,a):
-    b=input('enter a name')
-    d=int(input('enter a age'))
-    c=int(input('enter a roll no'))
-    l.append({'name':b,'age':d,'roll no':c})
+# l=[]
+# a=int(input('number of students'))
+# for i in range(0,a):
+#     b=input('enter a name')
+#     d=int(input('enter a age'))
+#     c=int(input('enter a roll no'))
+#     l.append({'name':b,'age':d,'roll no':c})
     
-print(l)
+# print(l)
 
-for i in l:
-    print(i)
+# for i in l:
+#     print(i)
+
+
+#programme runnig
+student=[]
+while True:
+    print("""
+1.Register
+2.View
+3.Update
+4.Delete
+5.Search
+6.exit""")
+    ch=int(input("Enter your choice"))
+    if ch==1:
+        if len(student)==0:
+            adm=1
+        else:
+            adm=student[-1]['adm_no']+1
+        name=input("Enter name:")
+        age=int(input("Enter age:"))
+        cur=input("Enter course:")
+        fee=int(input("Enter fee:"))
+        student.append({"adm_no":adm,"name":name,"age":age,"course":cur,"fee":fee})
+    elif ch==2:
+        print('{:<10}{:<20}{:<10}{:<20}{:<10}' .format('adm_no','name','age','course','fee'))#for formating the output to a table structure
+        print('-'*70)
+        for std in student:
+            print('{:<10}{:<20}{:<10}{:<20}{:<10}' .format(std['adm_no'],std['name'],std['age'],std['course'],std['fee']))#for formating the output to a table structure
+
+        
+    elif ch==2:
+        print(student)
+    elif ch==3:
+        print("Update")
+    elif ch==4:
+        print("Delete")
+    elif ch==5:
+        print("search")
+    elif ch==6:
+        print('exit')
+        break
+    else:
+        print("invalied choice")
 
 
