aca7a6c0b3628b493b28631881cfdca66875e7ee
kavya <kavyakkavyak153@gmail.com>
2025-04-06 16:17:07 +0530

    initial command

---
+++ set_prg.py
@@ -0,0 +1,64 @@
+# def fun_name():
+    #stmts
+    #block of code
+
+    #funtion defined
+# def data():
+#     print("hello")
+#     print("world")
+#     print("python")
+
+# #funtion call
+# data()
+# print("function defined")
+# data()
+# print("funtion completed")
+# data()
+
+# a=10 #global variable#global variable was usig only in out of funtion
+
+# def data():
+#     b=20 #local variable
+#     a=25
+#     global c
+#     c=5
+#     print("hello",a)
+#     print("hello",b)
+
+# print("before fun",a)
+# data()
+# print("after fun",c)
+
+# a="welcome"
+# b=len(a)
+# print(b)
+
+# def demo():
+    #print("hai")
+#     return 'hello','world'
+
+# a,b=demo()
+# print(a)
+
+#types of arguiment
+#1.positional arguiment
+
+# def data(a,b):
+#     print(a)
+# data('hello',123)
+
+# def data(name,age):
+#     print('name',age)
+#     print('age',age)
+
+# data('manu',20)
+# data('ram',22)
+# data(age=20,name='akhil')
+
+
+
+
+
+
+
+
